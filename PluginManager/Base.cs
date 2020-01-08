2be96f10828ec212b398d767c586f4fa4d45d39d
zer0xday <majcher10@gmail.com>
2020-01-08 19:41:40 +0100

    Feat: add PluginManager library basic structure

---
+++ PluginManager/Base.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginManager
+{
+    public class Base
+    {
+        public void SendMessage(string messageContent, string conversationId, string[] receivers)
+        {
+            
+        } 
+
+        public Tuple <string, string, string> GetMessage()
+        {
+            var message = new Tuple<string, string, string>(
+                    "", "", ""
+                );
+            return message;
+        }
+    }
+}

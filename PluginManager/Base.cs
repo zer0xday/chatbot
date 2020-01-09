d35b5d677b80aeedf4ad14a6e300aaf843a0bb01
zer0xday <majcher10@gmail.com>
2020-01-09 19:45:24 +0100

    Add plugin manager dll library

--- PluginManager/Base.cs
+++ PluginManager/Base.cs
@@ -8,17 +8,36 @@ namespace PluginManager
 {
     public class Base
     {
+        private string GetAnswer(string messageContent)
+        {
+            string answer = "";
+
+            switch(messageContent)
+            {
+                default: break;
+            }
+
+            return answer;
+        }
+
         public void SendMessage(string messageContent, string conversationId, string[] receivers)
         {
-            
+            string message = GetAnswer(messageContent);
+
         } 
 
         public Tuple <string, string, string> GetMessage()
         {
-            var message = new Tuple<string, string, string>(
+            var message = new Tuple<string, string, string>
+                (
                     "", "", ""
                 );
             return message;
+        }
+
+        public void setBotName(string name)
+        {
+
         }
     }
 }

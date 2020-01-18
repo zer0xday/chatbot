0696d97e678cd371f01d25e13a177093953ee956
Jarosław Żak <shado.ackerly@gmail.com>
2020-01-18 18:29:26 +0100

    Checking logged in

--- PolfanConnector/Connection.cs
+++ PolfanConnector/Connection.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using Websocket.Client;
 
 
@@ -8,9 +7,16 @@ namespace PolfanConnector
 {
     public class Connection
     {
+        public bool IsLoggedIn
+        {
+            get { return this.isLoggedIn; }
+            set { }
+        }
+
         protected WebsocketClient wsClient;
         protected Queue<Message> receivedMessages = new Queue<Message>();
         protected ConnectionSettings settings;
+        protected bool isLoggedIn = false;
 
         public void Connect(ConnectionSettings settings)
         {
@@ -19,6 +25,7 @@ namespace PolfanConnector
             wsClient = new WebsocketClient(settings.Uri);
             wsClient.ReconnectionHappened.Subscribe(type => handleReconnect(type.Type));
             wsClient.MessageReceived.Subscribe(msg => handleIncomingFrame(msg.ToString()));
+            wsClient.DisconnectionHappened.Subscribe(ev => handleDisconnect());
             wsClient.Start();
         }
 
@@ -47,24 +54,29 @@ namespace PolfanConnector
             var message = new Message();
             message.SetJson(frame);
 
-            try
+            switch (message.GetInt(0))
             {
-                if (message.GetInt(0) == 1)
-                {
+                case 1: // ping
                     sendPong();
-                    return;
-                }
-            } 
-            catch (IndexOutOfRangeException)
-            { }
-
-            receivedMessages.Enqueue(message);
+                    break;
+                case 626: // loggedIn
+                    this.isLoggedIn = true;
+                    break;
+                default:
+                    receivedMessages.Enqueue(message);
+                    break;
+            }
         }
 
         protected void handleReconnect(ReconnectionType type)
         {
             Console.WriteLine($"handleReconnect: {type}, {settings.UriString}, {settings.Nick}, {settings.Room}");
             sendLogIn();
+        }
+
+        protected void handleDisconnect()
+        {
+            this.isLoggedIn = false;
         }
 
         protected void sendLogIn()

88e491ea9138873a2c45cadd22966d1f740bd85c
Jarosław Żak <shado.ackerly@gmail.com>
2020-01-18 14:37:55 +0100

    Remove unused test project

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using PolfanConnector;
-using System.Threading.Tasks;
+
 
 namespace PolfanIOPluginTests
 {
@@ -8,50 +8,10 @@ namespace PolfanIOPluginTests
     {
         static void Main(string[] args)
         {
-            var settings = new ConnectionSettings()
-            {
-                UriString = "ws://s1.polfan.pl:14080",
-                Nick = "aaaaa"
-            };
-
-            var connection = new Connection();
-
-
-
-            connection.Connect(settings);
-
-            Console.WriteLine("Logujemy");
-
-            var message = new Message();
-            message
-                .AddInt(1400)
-                .AddString("aaaaa", "aaaaa", "gabinet", "", "", "", "c#ioplugin");
-
-            connection.SendMessage(message);
-
-            wri(connection).Wait();
+            //var plugin = new Plugin();
 
 
         }
 
-        static async Task wri(Connection connection)
-        {
-            await Task.Run(() =>
-            {
-                while (true)
-                {
-                    var message = connection.ReceiveMessage();
-
-                    if (message != null)
-                    {
-                        Console.WriteLine(message);
-                    }
-
-
-
-                    
-                }
-            });
-        }
     }
 }

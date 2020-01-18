416f04707071f96fd71abe355562a7724c7d5493
Jarosław Żak <shado.ackerly@gmail.com>
2020-01-18 14:39:47 +0100

    New polfan connector test project app

---
+++ PolfanConnectorTester/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PolfanConnector;
+
+namespace PolfanConnectorTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var plugin = new Plugin();
+
+            plugin.Init("bocik");
+
+            while (plugin.IsConnected())
+            {
+                var message = plugin.GetMessage();
+
+                if (message == null)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("WIADOMOSC:" + message);
+
+            }
+
+        }
+    }
+}

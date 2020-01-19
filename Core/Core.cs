3717f85f29be4a79eb34b20b1c85aedc8fdee71a
Jarosław Żak <shado.ackerly@gmail.com>
2020-01-19 14:32:58 +0100

    Core: initializing plugin

--- Core/Core.cs
+++ Core/Core.cs
@@ -1,16 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
+using PolfanConnector;
 
 namespace Core
 {
     public class Core
     {
-        static void Main()
+        private const int IO_PLUGIN_INIT_TIMEOUT_SEC = 10;
+
+        private dynamic pluginInstance;
+
+        public Core()
         {
             PluginValidator pluginValidator = new PluginValidator();
 
             // Validate referenced plugins
             pluginValidator.Validate();
+
+            
+        }
+
+        public void LoadPlugin()
+        {
+            var endTime = DateTime.Now.AddSeconds(IO_PLUGIN_INIT_TIMEOUT_SEC);
+
+            //później trzeba wywalić PolfanConnector z odwołań - ma się ładować na żądanie wg konfiguracji
+            pluginInstance = new Plugin();
+            pluginInstance.Init();
+
+            while (!pluginInstance.IsReady) // asynchronicznie?
+            {
+                if (DateTime.Now >= endTime)
+                {
+                    throw new TimeoutException($"IO plugin initializing timeout: {IO_PLUGIN_INIT_TIMEOUT_SEC}s");
+                }
+            }
         }
     }
 }

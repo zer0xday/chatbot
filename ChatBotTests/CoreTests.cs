6fd97dad252efb6fd578dbb32806e586547668c6
Jarosław Żak <shado.ackerly@gmail.com>
2020-01-25 20:10:20 +0100

    Unit tests for Core

---
+++ ChatBotTests/CoreTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatBot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBot.Tests
+{
+    [TestClass()]
+    public class CoreTests
+    {
+        [TestMethod()]
+        public void SendMessageTest()
+        {
+            var core = new Core();
+            var message = "test string";
+
+            Assert.ThrowsException<Core.PluginNotReadyException>(() =>
+            {
+                core.SendMessage(message);
+            });
+        }
+
+        [TestMethod()]
+        public void DisconnectTest()
+        {
+            var core = new Core();
+
+            Assert.ThrowsException<Core.PluginNotReadyException>(() =>
+            {
+                core.Disconnect();
+            });
+        }
+    }
+}

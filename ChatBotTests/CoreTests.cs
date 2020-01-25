d99bf23da3419d548b758c53860dfc9018fd1ec5
Jarosław Żak <shado.ackerly@gmail.com>
2020-01-25 21:25:04 +0100

    OnStateChange, OnSystemMessage tests

--- ChatBotTests/CoreTests.cs
+++ ChatBotTests/CoreTests.cs
@@ -3,12 +3,15 @@ using ChatBot;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ChatBot.Tests
 {
     [TestClass()]
     public class CoreTests
     {
+        public string PLUGIN_PATH = @"C:\Users\Szado\source\repos\cs-college-app\plugins\PolfanConnector.dll";
+
         [TestMethod()]
         public void SendMessageTest()
         {
@@ -30,6 +33,39 @@ namespace ChatBot.Tests
             {
                 core.Disconnect();
             });
+        }
+
+        [TestMethod()]
+        [Timeout(10000)]
+        public void OnStateChangeTest()
+        {
+            var core = new Core();
+            var stop = false;
+            core.OnStateChange = (bool isReady) =>
+            {
+                stop = true;
+                Assert.IsTrue(isReady);
+            };
+
+            core.Connect("Bocik12321", PLUGIN_PATH);
+            
+            while (!stop) { }
+        }
+
+        [TestMethod()]
+        public void OnMessageTest()
+        {
+            var core = new Core();
+            var stop = false;
+            core.OnSystemMessage = (string message) =>
+            {
+                stop = true;
+                Assert.IsTrue(message.Length > 0);
+            };
+
+            core.Connect("Bocik32123", PLUGIN_PATH);
+
+            while (!stop) { }
         }
     }
 }
